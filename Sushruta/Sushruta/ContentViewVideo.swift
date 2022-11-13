9765b4193c84bc845752d246c537fe57316fd32d
daedluz <hiiammichael0520@gmail.com>
2022-11-13 16:22:31 +0800

    feat: Charts on video page

--- Sushruta/Sushruta/ContentViewVideo.swift
+++ Sushruta/Sushruta/ContentViewVideo.swift
@@ -6,24 +6,39 @@
 //
 
 import SwiftUI
+import GanttisTouch
 import PhotosUI
+import Charts
 
 
 struct ContentViewVideo: View {
     
     @State var Start = false
+    @State private var selectedPhotoData: Data?
     
     @State var isPlaying = false
     @State var showPhaseDetail = false
     @State var showFinalReport = false
+    
+    @State var items = [GanttChartViewItem]()
+    @State var dependencies = [GanttChartViewDependency]()
+    @State var rowHeaders: [String?]? = nil
+    @State var schedule = Schedule.continuous
+    //    @State var theme = Theme.jewel
+    @State var theme = Theme.standard
+    
+    @State var lastChange = "none"
+    @State var dateToAddTo = 2
+    
     @State var showHistoryMessage = false
     @State var finish = true
     @State private var selectedItem: PhotosPickerItem?
-    @State private var selectedPhotoData: Data?
-
+    
+    let labels = (1...10).map { "Phase" + String($0) }
+    
     
     var body: some View {
-
+        
         VStack(spacing:0){
             Text("Sushruta")
                 .font(.largeTitle)
@@ -31,10 +46,10 @@ struct ContentViewVideo: View {
             
             Divider()
                 .padding(.top,10)
-
-
             
-
+            
+            
+            
             HStack{
                 
                 
@@ -86,21 +101,21 @@ struct ContentViewVideo: View {
                     
                     
                     
-                     // Start and Pause Button
-//                    Button(action: {
-//                        isPlaying.toggle()
-//                        if isPlaying {
-////                                        player.pause()
-//                        } else {
-////                                        player.play()
-//                        }
-//                    }) {
-//                        Image(systemName: isPlaying ? "pause" : "play.fill")
-//                            .padding(3.0)
-//                            .border(Color.accentColor, width: 2)
-//                    }
-//                    .frame(width: 0.0, height: 0.0)
-//                    .font(.system(size:34))
+                    // Start and Pause Button
+                    //                    Button(action: {
+                    //                        isPlaying.toggle()
+                    //                        if isPlaying {
+                    ////                                        player.pause()
+                    //                        } else {
+                    ////                                        player.play()
+                    //                        }
+                    //                    }) {
+                    //                        Image(systemName: isPlaying ? "pause" : "play.fill")
+                    //                            .padding(3.0)
+                    //                            .border(Color.accentColor, width: 2)
+                    //                    }
+                    //                    .frame(width: 0.0, height: 0.0)
+                    //                    .font(.system(size:34))
                     
                     
                     
@@ -137,7 +152,7 @@ struct ContentViewVideo: View {
                     }
                     .listStyle(.plain)
                     .listRowSeparatorTint(.purple)
-                .offset(y:-5)
+                    .offset(y:-5)
                 }
                 
                 
@@ -221,32 +236,127 @@ struct ContentViewVideo: View {
             }
             .padding(5.0)
             
-
+            
             
             
             HStack{
                 
-                // gannt graph
+                // Gantt Chart
                 VStack{
-                    
                     HStack {
-                        Text("Gantt Graph")
+                        Text("Gantt Chart")
                             .font(.headline)
-                            .padding(0.0)
-                            
                         Spacer()
                     }
-                    HStack {
-                        Image("ganntsample")
-                            .resizable()
-//                            .frame(width: 550, height: 300)
-                            .cornerRadius(10.0)
-                        Spacer()
-                    }
-                    .offset(y:-10)
+                    GanttChartView(
+                        items: $items,
+                        dependencies: $dependencies,
+                        schedule: schedule,
+                        headerRows: [
+                            GanttChartHeaderRow(TimeSelector(.seconds))
+                        ],
+                        rowHeight: 50,
+                        hourWidth: 21600,
                         
+                        scrollableTimeline: TimeRange(from: Time.current.dayStart,
+                                                      to: Time.current.adding(hours: 10)),
+                        
+                        //                scheduleHighlighters: [ScheduleTimeSelector(.weekends)],
+                        //                intervalHighlighters: [TimeSelector(.weeks(startingOn: .monday)), TimeSelector(.time)],
+                        //                timeScale: .intervalsWith(period: 15, in: .minutes),
+                        desiredScrollableRowCount: 5,
+                        rowHeaders: rowHeaders,
+                        rowHeadersWidth: 150,
+                        theme: theme,
+                        onItemAdded: { item in
+                            lastChange = "\(item.label ?? "item") added"
+                        },
+                        onItemRemoved: { _ in
+                            lastChange = "item removed"
+                        },
+                        onDependencyAdded: { dependency in
+                            lastChange = "dependency added from \(dependency.from(considering: items)!.label ?? "item") to \(dependency.to(considering: items)!.label ?? "item")"
+                        },
+                        onDependencyRemoved: { _ in
+                            lastChange = "dependency removed"
+                        },
+                        onTimeChanged: { item, _ in
+                            lastChange = "time updated for \(item.label ?? "item")"
+                        },
+                        onCompletionChanged: { item, _ in
+                            lastChange = "completion updated for \(item.label ?? "item")"
+                        },
+                        onRowChanged: { item, _ in
+                            lastChange = "row updated for \(item.label ?? "item")"
+                        }
+                    )
+                    
+                    
                 }
-                Spacer()
+                .toolbar {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Button("Add new") {
+                            addNewItem()
+                            lastChange = "added new item"
+                            dateToAddTo += 1
+                        }
+                        Button("Update") {
+                            updateAnItem()
+                            lastChange = "updated an item"
+                        }
+                        Button("Remove dep.") {
+                            removeAllDependencies()
+                            lastChange = "removed all dependencies"
+                        }
+                        Spacer()
+                        Button("Theme") {
+                            changeTheme()
+                        }
+                    }
+                }
+                .onAppear() {
+                    let color_list = [Color.darkGreen, Color.lightBlue, Color.red, Color.orange, Color.black, Color.darkBlue]
+                    var item_list = [
+                        GanttChartViewItem(label: "A", row: 0, start: date(1), finish: date(2)),
+                        GanttChartViewItem(label: "A", row: 0, start: date(10), finish: date(15)),
+                        GanttChartViewItem(label: "B", row: 1, start: date(1), finish: date(2)),
+                        GanttChartViewItem(label: "C", row: 1, start: date(4), finish: date(5)),
+                        GanttChartViewItem(row: 1, start: date(10), finish: date(11)),
+                        GanttChartViewItem(row: 2, start: date(2), finish: date(4))]
+                    item_list[0].style.barFillColor = .darkGreen
+                    item_list[4].details = "Special item"
+                    item_list[1].completion = 1
+                    item_list[2].completion = 0.25
+                    item_list[2].type = .summary
+                    item_list[4].completion = 0.08
+                    for i in 3..<500 {
+                        let tmp = Int.random(in:0..<6)
+                        if (tmp == item_list.last?.row) {
+                            item_list[item_list.count-1].finish = item_list.last?.finish.adding(seconds: 1) ?? Time().dayStart
+                        }
+                        else {
+                            item_list.append(GanttChartViewItem(row: tmp,
+                                                                start: date(i), finish: date(i + 1)))
+                            
+                            item_list[item_list.count-1].style.barFillColor = color_list[tmp]
+                        }
+                        
+                    }
+                    
+                    self.items = item_list.filter { $0.finish.second -  $0.start.second  > 1 }
+                    self.dependencies = dependencies
+                    let rowHeaders = [
+                        "grasper",
+                        "hook",
+                        "scissors",
+                        "clipper",
+                        "irrigator"
+                    ]
+                    self.rowHeaders = rowHeaders
+                    
+                    
+                }
+                
                 // instrument displacement ratio
                 VStack{
                     HStack {
@@ -255,20 +365,38 @@ struct ContentViewVideo: View {
                         Spacer()
                     }
                     HStack {
-                        Image("instrumentdisplacesample")
-                            .resizable()
-//                            .frame(width: 550, height: 300)
-                            .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                        VStack {
+                            SimpleLineChartView()
+                        }
+                        .padding()
                         Spacer()
                     }
                     .offset(y:-10)
-                        
+                    
                 }
                 
             }
             .padding(5.0)
             .padding(.bottom, 5.0)
         }
+        
+    }
+    
+    func addNewItem() {
+        let start = Int.random(in:0..<500)
+        let end = Int.random(in:start..<500)
+        items.append(
+            GanttChartViewItem(label: "New", row: Int.random(in:0..<6),
+                               start: date(start), finish: date(end)))
+    }
+    func updateAnItem() {
+        items[1].label = "Updated at :\(String(format: "%02d", Time.current.second))"
+    }
+    func removeAllDependencies() {
+        dependencies.removeAll()
+    }
+    func changeTheme() {
+        theme = theme == .standard ? .jewel : .standard
     }
 }
 
